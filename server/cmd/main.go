7391fcc41b33c5e6e4d21b6be316167812849700
daewood <daewood@qq.com>
2019-08-03 17:00:24 +0100

    Update main.go

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -57,7 +57,7 @@ func RegisterToConsul() {
 //health
 type HealthImpl struct{}
 
-// Check 实现健康检查接口，这里直接返回健康状态，这里也可以有更复杂的健康检查策略，比如根据服务器负载来返回
+// Check
 func (h *HealthImpl) Check(ctx context.Context, req *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
 	fmt.Print("health checking\n")
 	return &grpc_health_v1.HealthCheckResponse{
